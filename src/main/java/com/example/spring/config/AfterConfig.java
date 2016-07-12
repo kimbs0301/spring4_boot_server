465f6286b8a6ce59754203052f0a4dcf79d19156
김병수 <seban21@aceproject.co.kr>
2016-07-12 18:28:40 +0900

    ok

--- src/main/java/com/example/spring/config/AfterConfig.java
+++ src/main/java/com/example/spring/config/AfterConfig.java
@@ -1,10 +1,14 @@
 package com.example.spring.config;
 
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.env.Environment;
@@ -29,12 +33,21 @@ public class AfterConfig {
 		LOGGER.debug("생성자 AfterConfig()");
 	}
 
+	@Bean
+	public String sqlSessionFactory(ApplicationContext applicationContext) throws Exception {
+		Map<String, Object> map = applicationContext.getBeansWithAnnotation(Configuration.class);
+		for (String key : map.keySet()) {
+			LOGGER.debug("{} {}", key, map.get(key));
+		}
+		return "ok";
+	}
+
 	@PostConstruct
 	public void init() {
 		LOGGER.debug("");
-		
+
 		readThreadPool.startPool();
-		
+
 		String[] activeProfiles = environment.getActiveProfiles();
 		for (String activeProfile : activeProfiles) {
 			LOGGER.debug("{}", activeProfile);
